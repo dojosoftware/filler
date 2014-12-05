5d9e06cec0fc32734329e97549e88a6e3fcbadbf
Dragan Jovanović <draganjovanovic1@gmail.com>
2014-12-05 19:57:04 +0100

    Initial commit

---
+++ src/Filler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("In general, a filler is something that is used to fill gaps");
+
+            Console.ReadKey();
+        }
+    }
+}
